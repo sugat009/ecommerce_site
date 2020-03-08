2bb0a85c8d78782853a7ecac45a7f401059864dd
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-08 19:43:25 +0545

    Sign up page added and sign in component created, sign up still left

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./sign-in-and-sign-up.styles.scss";
+import SignIn from "../../sign-in/sign-in.component";
+
+const SignInAndSignUpPage = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      SIGN IN
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUpPage;
---
+++ src/sign-in/sign-in.component.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+import "./sign-in.styles.scss";
+import FormInput from '../components/form-input/form-input.component';
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({
+      email: "",
+      password: ""
+    });
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form action="" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="Email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="Password"
+            required
+          />
+
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
