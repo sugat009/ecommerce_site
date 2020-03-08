70870d524effa271590336caaf3129bbc0e67ded
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-08 11:31:17 +0545

    Shop page added and collection-preview component added with some dummy data

--- src/App.js
+++ src/App.js
@@ -2,23 +2,16 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 import "./App.css";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>Hats page</h1>
-    </div>
-  );
-};
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
