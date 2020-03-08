35bcff048f8b78e7ba8cd5844a4e85ee82b25661
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-08 10:08:16 +0545

    routing basics added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import Homepage from './pages/homepage/homepage.component';
-import './App.css';
+import Homepage from "./pages/homepage/homepage.component";
+
+import "./App.css";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import Directory from "../../components/directory/directory.component";
 
-import './homepage.styles.scss';
+import "./homepage.styles.scss";
 
 const Homepage = () => {
   return (
     <div className="homepage">
-        <Directory />
+      <Directory />
     </div>
   );
 };
