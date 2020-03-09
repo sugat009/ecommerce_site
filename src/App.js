99300dcb98aebc03b61202c6fd4b248315afb742
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-09 12:51:36 +0545

    firebase authenticated user added to state of our app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, {Component} from "react";
 import {Route, Switch} from "react-router-dom";
 
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import {auth} from './firebase/firebase.utils';
 
 import "./App.css";
 
-function App() {
-    return (
-        <div>
-            <Header/>
-            <Switch>
-                <Route exact path="/" component={Homepage}/>
-                <Route path="/shop" component={ShopPage}/>
-                <Route path="/signin" component={SignInAndSignUpPage}/>
-            </Switch>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            currentUser: null
+        }
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+            this.setState({
+                currentUser: user
+            })
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Switch>
+                    <Route exact path="/" component={Homepage}/>
+                    <Route path="/shop" component={ShopPage}/>
+                    <Route path="/signin" component={SignInAndSignUpPage}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
