779ef57444607f8eca3da1a8ed5167fbfecf206f
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-14 16:58:31 +0545

    Header component fully setup with redux and many other features of react-redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
 import {Route, Switch} from "react-router-dom";
+import {connect} from 'react-redux';
 
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {setCurrentUser} from "./redux/user/user.actions";
 
 import "./App.css";
 
@@ -13,15 +15,9 @@ class App extends Component {
     // Subscription for the user authentication
     unsubscribeFromAuth = null;
 
-    constructor() {
-        super();
-
-        this.state = {
-            currentUser: null
-        }
-    }
-
     componentDidMount() {
+        const {setCurrentUser} = this.props;
+
         // When the component is mounted it checks whether or not the user is signed in
         // or signed out
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
@@ -31,18 +27,14 @@ class App extends Component {
 
                 // Setting the currentUser state using the snapShot obtained from firebase
                 userRef.onSnapshot(snapshot => {
-                    this.setState({
-                        currentUser: {
-                            id: snapshot.id,
-                            ...snapshot.data()
-                        }
+                    setCurrentUser({
+                        id: snapshot.id,
+                        ...snapshot.data()
                     })
                 });
             } else {
                 // In case of logout setting current user as null
-                this.setState({
-                    currentUser: userAuth
-                });
+                setCurrentUser(userAuth);
             }
         });
     }
@@ -56,7 +48,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <Header currentUser={this.state.currentUser}/>
+                <Header/>
                 <Switch>
                     <Route exact path="/" component={Homepage}/>
                     <Route path="/shop" component={ShopPage}/>
@@ -67,4 +59,13 @@ class App extends Component {
     }
 }
 
-export default App;
+// mapDispatchToProps is a function that is used for dispatching actions to the store
+// the argument {dispatch} in this function actually calls the root reducer with the object
+// before reducing, we call our action function to get the proper object that is needed by
+// the reducer in question, in this case the setCurrentUser
+const mapDispatchToProps = dispatch => ({
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+// Since we dont need state as props in this component we are setting the first argument as null
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {connect} from "react-redux";
 
 import "./header.styles.scss";
 import {Link} from "react-router-dom";
@@ -32,4 +33,10 @@ const Header = ({currentUser}) => {
     );
 };
 
-export default Header;
+// here state is the top level root reducer
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser
+});
+
+// connect() is a HOC that modifies our component to have access to the redux objects
+export default connect(mapStateToProps)(Header);
