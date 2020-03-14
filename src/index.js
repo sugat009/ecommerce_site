dc3ddd2024f41d065866038289e2555ee92663e9
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-14 16:12:31 +0545

    setCurrentUser action function written and redux store configured with redux-logger middleware and linked with Provider component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
+import store from "./redux/store";
+
+import "./index.css";
+import App from "./App";
+
+
 ReactDOM.render(
-    <Provider>
+    // Provider is the main component object from react-redux that takes in the redux store
+    // and is able to provide the store to all components in the app
+    <Provider store={store}>
         <BrowserRouter>
             <App/>
         </BrowserRouter>
---
+++ src/redux/user/user.actions.js
@@ -0,0 +1,8 @@
+// Action is a function that gets fired when an action is performed in the UI
+// It gets an argument needed of change which then, fires the appropriate reducer
+// NOTE: the type of this action function is exactly same as that of user.reducer.js
+// because this action needs to work only with the user.reducer.js
+export const setCurrentUser = user => ({
+    type: "SET_CURRENT_USER",
+    payload: user
+});
