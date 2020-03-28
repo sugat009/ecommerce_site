bbbe66b0ff68cb3a293c7436f3ae7ca6771ad920
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-28 12:14:26 +0545

    persistence of cart items added using redux-persist

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
 
-import store from "./redux/store";
+import {persistor, store} from "./redux/store";
 import "./index.css";
 import App from "./App";
 
@@ -13,7 +14,9 @@ ReactDOM.render(
     // and is able to provide the store to all components in the app
     <Provider store={store}>
         <BrowserRouter>
-            <App/>
+            <PersistGate persistor={persistor}>
+                <App/>
+            </PersistGate>
         </BrowserRouter>
     </Provider>,
     document.getElementById("root")
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,13 +1,27 @@
 import {combineReducers} from "redux";
+import {persistReducer} from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-// This function is from redux library that combines all the reducers into one big reducer
-// The key are the value in the redux store whereas the value are the respective reducers
-// called when necessary
+// A JS object that contains the comfigurations for the persistor
+// Whitelist key is the list of reducers in string for which to persist
+// storage is the storage in use
+// key is the root
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+};
 
-export default combineReducers({
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+// This function is from redux library that combines all the reducers into one big reducer
+// The key are the value in the redux store whereas the value are the respective reducers
+// called when necessary
+export default persistReducer(persistConfig, rootReducer);
+// root reducer with persistence
