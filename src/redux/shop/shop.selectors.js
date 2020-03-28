4cb09ce4a5fa7cc66f630a469e91406fceee3803
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-28 17:42:02 +0545

    Shop data changed from array to object which caused shop page to not work fixed by creating new selector that changes hash back to array as expected by the component

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,6 +7,11 @@ export const selectCollections = createSelector(
     shop => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+);
+
 // createSelector takes in the state and returns a function which basically returns collection in this case
 export const selectCollection = collectionUrlParam => createSelector(
     [selectCollections],
