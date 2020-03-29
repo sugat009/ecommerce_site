59ea2408b038964c2d99f03abc91629a6b457def
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-29 18:12:06 +0545

    CollectionPreview Component changed to Styled Component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,20 +2,23 @@ import React from "react";
 
 import CollectionItem from "../collection-item/collection-item.components";
 
-import "./collection-preview.styles.scss";
+import {CollectionPreviewContainer, PreviewContainer, TitleContainer} from "./collection-preview.styles";
 
 const CollectionPreview = ({title, items}) => {
     return (
-        <div className="collection-preview">
-            <h1 className="title">{title.toUpperCase()}</h1>
-            <div className="preview">
+        <CollectionPreviewContainer>
+            <TitleContainer>{title.toUpperCase()}</TitleContainer>
+            <PreviewContainer>
                 {items
                     .filter((item, idx) => idx < 4)
                     .map(item => (
-                        <CollectionItem key={item.id} item={item}/>
+                        <CollectionItem
+                            key={item.id}
+                            item={item}
+                        />
                     ))}
-            </div>
-        </div>
+            </PreviewContainer>
+        </CollectionPreviewContainer>
     );
 };
 
