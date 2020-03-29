96dd58fc709a13ecd4093bd4644681cbf03bc3a0
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-29 14:36:21 +0545

    CartIcon Component changed to Styled Component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,15 +5,14 @@ import {toggleCartHidden} from "../../redux/cart/cart.actions";
 import {selectCartItemsCount} from "../../redux/cart/cart.selectors";
 import {createStructuredSelector} from "reselect";
 
-import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg";
-import "./cart-icon.styles.scss";
+import {CartContainer, ItemCountContainer, ShoppingIcon} from "./cart-icon.styles";
 
 const CartIcon = ({toggleCartHidden, itemCount}) => {
     return (
-        <div className="cart-icon" onClick={toggleCartHidden}>
-            <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">{itemCount}</span>
-        </div>
+        <CartContainer onClick={toggleCartHidden}>
+            <ShoppingIcon/>
+            <ItemCountContainer>{itemCount}</ItemCountContainer>
+        </CartContainer>
     );
 };
 
