6de9bf7642f281f22ec02b9ae9ca058739fc63bc
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-29 21:28:45 +0545

    Shop data stored in firebase and made to fetch via the ShopComponent and storing in redux but the initial state from shop.data.js yet to be removed

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,52 @@
-import React from "react";
+import React, {Component} from "react";
 import {Route} from "react-router-dom";
+import {connect} from "react-redux";
 
 import CollectionPage from "../collection/collection.component";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebase.utils";
+import {updateCollections} from "../../redux/shop/shop.actions";
 
-const ShopPage = ({match}) => {
-    return (
-        <div className="shop-page">
-            {/* This concept is known as nested routing */}
-            {/* here the match props is passed by the Route component along with history and some other thing*/}
-            {/* but we only need match in this case */}
-            {/* This is done to ensure that the component is reusable and also that ShopPage component*/}
-            {/* does not need to know which route it lies in */}
-            <Route
-                exact
-                path={`${match.path}`}
-                component={CollectionsOverview}
-            />
-            {/* NOTE: collectionID is not integer but a string like hats, jackets, etc. */}
-            <Route
-                path={`${match.path}/:collectionId`}
-                component={CollectionPage}
-            />
-        </div>
-    );
-};
+class ShopPage extends Component {
+    unsubscribeFromSnapshot = null;
 
-export default ShopPage;
+    componentDidMount() {
+        const {updateCollections} = this.props;
+        const collectionRef = firestore.collection('collections');
+
+        collectionRef.onSnapshot(async snapshot => {
+            updateCollections(convertCollectionsSnapshotToMap(snapshot));
+        });
+    }
+
+    render() {
+        const {match} = this.props;
+
+        return (
+            <div className="shop-page">
+                {/* This concept is known as nested routing */}
+                {/* here the match props is passed by the Route component along with history and some other thing*/}
+                {/* but we only need match in this case */}
+                {/* This is done to ensure that the component is reusable and also that ShopPage component*/}
+                {/* does not need to know which route it lies in */}
+                <Route
+                    exact
+                    path={`${match.path}`}
+                    component={CollectionsOverview}
+                />
+                {/* NOTE: collectionID is not integer but a string like hats, jackets, etc. */}
+                <Route
+                    path={`${match.path}/:collectionId`}
+                    component={CollectionPage}
+                />
+            </div>
+        );
+    }
+}
+
+
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
