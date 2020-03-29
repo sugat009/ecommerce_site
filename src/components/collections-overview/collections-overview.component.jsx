ae22bd496f52acc8b9d6f790e55521efd6c1b5b0
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-29 18:13:50 +0545

    CollectionOverview Component changed to Styled Component

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -5,15 +5,15 @@ import {createStructuredSelector} from "reselect";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 import {selectCollectionsForPreview} from "../../redux/shop/shop.selectors";
 
-import "./collections-overview.styles.scss";
+import {CollectionsOverviewContainer} from "./collections-overview.styles";
 
 const CollectionsOverview = ({collections}) => {
     return (
-        <div className="collections-overview">
+        <CollectionsOverviewContainer>
             {collections.map(({id, ...otherCollectionProps}) => (
                 <CollectionPreview key={id} {...otherCollectionProps} />
             ))}
-        </div>
+        </CollectionsOverviewContainer>
     );
 };
 
