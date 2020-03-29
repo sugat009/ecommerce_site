69b754992c5d47c874d1d0ef862bd3466f3df3d1
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-29 14:31:38 +0545

    CartDropDown Component changed to Styled Component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,35 +3,43 @@ import {connect} from "react-redux";
 import {createStructuredSelector} from "reselect";
 import {withRouter} from "react-router-dom";
 
-import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 import {selectCartItems} from "../../redux/cart/cart.selectors";
 import {toggleCartHidden} from "../../redux/cart/cart.actions";
 
-import "./cart-dropdown.styles.scss";
+import {
+    CartDropdownButton,
+    CartDropDownContainer,
+    CartItemsContainer,
+    EmptyMessageContainer
+} from "./cart-dropdown.styles";
 
 // When mapDispatchToProps is not passed as argument to connect
 // it passes dispatch function as a prop to the component
 const CartDropdown = ({cartItems, history, dispatch}) => {
     return (
-        <div className="cart-dropdown">
-            <div className="cart-items">
+        <CartDropDownContainer>
+            <CartItemsContainer>
                 {
                     cartItems.length ?
                         cartItems.map(cartItem => (
-                            <CartItem key={cartItem.id} item={cartItem}/>
+                            <CartItem
+                                key={cartItem.id}
+                                item={cartItem}
+                            />
                         )) :
-                        <span className="empty-message">Your cart is empty</span>
+                        <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
                 }
-            </div>
-            <CustomButton
+            </CartItemsContainer>
+            <CartDropdownButton
                 onClick={() => {
                     history.push("/checkout");
                     dispatch(toggleCartHidden());
-                }}>
+                }}
+            >
                 GO TO CHECKOUT
-            </CustomButton>
-        </div>
+            </CartDropdownButton>
+        </CartDropDownContainer>
     );
 };
 
